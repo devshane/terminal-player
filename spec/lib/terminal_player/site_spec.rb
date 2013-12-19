b0bde9ea1cad160793601caa9fdf796328d98a6c
Shane Thomas <shane@devshane.com>
2013-12-19 09:26:06 -0500

    added site spec

---
+++ spec/lib/terminal_player/site_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+require 'terminal_player'
+
+describe Site do
+  it "bombs with no url" do
+    options = {cache: 999, cache_min: 12, url: ''}
+    lambda { Site.new(options, "di") }.should raise_error RuntimeError
+  end
+
+  it "sets up properly for di.fm" do
+    options = {cache: 999, cache_min: 12, url: 'http://www.di.fm/breaks.pls'}
+    s = Site.new(options, 'di')
+    s.name.should == 'di'
+    s.current_channel.should == 'breaks'
+    s.player.class.should be Mplayer
+  end
+
+  it "should return appropriate types for spotify URIs" do
+    options = {cache: 999, cache_min: 12, url: 'http://www.di.fm/breaks.pls'}
+    s = Site.new(options, 'di')
+    output = s.send(:spotify_type, 'spotify:album:5PFwYpKSHE3Zab4YTrgyv2')
+    output.should == 'album'
+
+    output = s.send(:spotify_type, 'spotify:playlist:5PFwYpKSHE3Zab4YTrgyv2')
+    output.should == 'playlist'
+
+    output = s.send(:spotify_type, 'spotify:track:5PFwYpKSHE3Zab4YTrgyv2')
+    output.should == 'track'
+
+    output = s.send(:spotify_type, 'spotify:unknown:5PFwYpKSHE3Zab4YTrgyv2')
+    output.should == 'spotify:unknown:5PFwYpKSHE3Zab4YTrgyv2'
+  end
+end
